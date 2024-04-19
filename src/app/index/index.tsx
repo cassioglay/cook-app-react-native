a36733852a1101c2b77c6341a488db786fada58e
cassioglay <cassioglay@MacBook-Pro-de-cassioglay.local>
2024-04-19 16:05:27 -0300

    chore: Fonts - add and config fonts

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -4,8 +4,12 @@ import { View, Text } from 'react-native'
 export default function Index(){
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Escolha os produtos</Text>
-            <Text style={styles.subtitle}> 
+            <Text style={styles.title}>
+                Escolha {"\n"}
+                
+                <Text style={styles.subtitle}>os produtos</Text>
+                </Text>
+            <Text style={styles.message}> 
                 Descubra a receitas baseadas nos produtos que você escolheu
             </Text>
         </View>
